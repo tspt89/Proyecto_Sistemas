15cf00de0d78982d0413a938e1d04dbfc2e87481
NMSamDev <A01730719@itesm.mx>
2021-06-05 21:34:12 -0500

    clasificador Gaussian Naive Bayes

--- generate2.py
+++ generate2.py
@@ -7,17 +7,38 @@ with open('./TrainingDS.csv', 'r') as csv_file:
     
     training = [] # Guarda diccionario entrada en una lista
     test = [] # Guarda diccionario entrada en una lista
-
+    allCases = []
     next(csv_reader)
     counter = 0
     for line in csv_reader:
-
+        
         entrada["ID"] = int(line[0])
+        # if line[3] == "N":
+        #     entrada["score_tag"] = 0
+        # elif line[3] == "P":
+        #     entrada["score_tag"] = 1
+        # elif line[3] == "NEU":
+        #     entrada["score_tag"] = 2
+        # elif line[3] == "NONE":
+        #     entrada["score_tag"] = 3
+        # if line[4] == "DISAGREEMENT":
+        #     entrada["agreement"] = 0
+        # elif line[4] == "AGREEMENT":
+        #     entrada["agreement"] = 1
+        # if line[5] == "SUBJECTIVE":
+        #   entrada["subjectivity"] = 0  
+        # elif line[5] == "OBJECTIVE":
+        #   entrada["subjectivity"] = 1
+        # if line[7] == "NONIRONIC":
+        #   entrada["irony"] = 0
+        # elif line[7] == "IRONIC":
+        #   entrada["irony"] = 1
+
         entrada["score_tag"] = line[3]
         entrada["agreement"] = line[4]
         entrada["subjectivity"] = line[5]
-        entrada["confidence"] = int(line[6])
         entrada["irony"] = line[7]
+        entrada["confidence"] = int(line[6])
         entrada["Happy"] = float(line[8])
         entrada["Fear"] = float(line[9])
         entrada["Sad"] = float(line[10])
@@ -25,6 +46,8 @@ with open('./TrainingDS.csv', 'r') as csv_file:
         entrada["Angry"] = float(line[12])
         entrada["Excited"] = float(line[13])
         entrada["Class"] = int(line[2])
+        
+        allCases.append(dict(entrada))
         if counter < 1200:
             training.append(dict(entrada))
         else:
@@ -40,11 +63,20 @@ with open('./TrainingDS.csv', 'r') as csv_file:
         for line in training:
             csv_writer.writerow(line)
     
-    with open('TestDS1.csv', 'w') as new_file:
+    with open('TestingDS1.csv', 'w') as new_file:
         headers = ["ID", "score_tag", "agreement", "subjectivity", "confidence", "irony", "Happy", "Fear", "Sad", "Bored", "Angry", "Excited", "Class"]
     
         csv_writer = csv.DictWriter(new_file, fieldnames=headers)
         csv_writer.writeheader()
 
         for line in test:
+            csv_writer.writerow(line)
+    
+    with open('AllCases1.csv', 'w') as new_file:
+        headers = ["ID", "score_tag", "agreement", "subjectivity", "confidence", "irony", "Happy", "Fear", "Sad", "Bored", "Angry", "Excited", "Class"]
+    
+        csv_writer = csv.DictWriter(new_file, fieldnames=headers)
+        csv_writer.writeheader()
+
+        for line in allCases:
             csv_writer.writerow(line)
